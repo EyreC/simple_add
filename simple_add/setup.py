aecde539b10ee55ac3cb443b92b08533c7345df2
Alexander Craggs <eyrecraggs@gmail.com>
2019-06-23 23:07:06 +0100

    Update dependencies

--- simple_add/setup.py
+++ simple_add/setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'matplotlib==3.1.0']
+requirements = ['Click>=6.0', 'matplotlib==2.3.3']
 
 setup_requirements = [ ]
 
